5dae6d2daa0a5b3003f3c90f55a467637ec440a9
davidASC4 <doctordan@live.com>
2017-07-12 10:27:24 -0400

    Func practice and rand() func

--- david.js
+++ david.js
@@ -1,7 +1,22 @@
-console.log("What");
-console.log("Are");
-console.log("You");
-console.log("Doing");
-console.log("In");
-console.log("My");
-console.log("Swamp");
+//Task
+//Create a rand() function
+//Takes a number
+//Returns a random number between 0 and that number
+
+//Psuedocode
+//multiply Math.random() by the given number
+//remove the decimal part
+//return the remaining intenger
+
+//Returns a random integer between 0 and num
+function rand(num){
+    var randomNum = num * Math.random();
+    var result = Math.floor(randomNum);
+    return result;
+}
+
+//console.log(rand(6));
+//console.log(rand(1000));
+//console.log(rand(2));
+//console.log(rand(0));
+
